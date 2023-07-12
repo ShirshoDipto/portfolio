8b1f986a9bac8c81dc142a3dab94871ebfc708e6
ShirshoDipto <shirshodipto@gmail.com>
2023-07-12 21:27:47 +0600

    Design about component

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,25 @@
+const Info = () => {
+  return (
+    <div className="aboutInfo grid">
+      <div className="aboutBox">
+        <i class="bx bx-award"></i>
+        <h3 className="aboutTitle">Experience</h3>
+        <span className="aboutSubtitle">8 Years Working</span>
+      </div>
+
+      <div className="aboutBox">
+        <i class="bx bx-briefcase"></i>
+        <h3 className="aboutTitle">Completed</h3>
+        <span className="aboutSubtitle">40+ Projects</span>
+      </div>
+
+      <div className="aboutBox">
+        <i class="bx bx-support"></i>
+        <h3 className="aboutTitle">Support</h3>
+        <span className="aboutSubtitle">Online 24 Hours</span>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
