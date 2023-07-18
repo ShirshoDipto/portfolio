efa79c6d6cc36f6c6cb5f1eb397d9e530c7f51d7
ShirshoDipto <shirshodipto@gmail.com>
2023-07-18 22:34:01 +0600

    Add animation to project section

--- src/components/project/ProjectItems.jsx
+++ src/components/project/ProjectItems.jsx
@@ -1,6 +1,15 @@
+import { useInView } from "react-intersection-observer";
+
 const ProjectItems = ({ item }) => {
+  const { ref, inView } = useInView({
+    threshold: 0,
+  });
+
   return (
-    <div className="projectCard">
+    <div
+      ref={ref}
+      className={inView ? "projectCard projectCardTransit" : "projectCard"}
+    >
       <img src={item.image} alt="" className="projectImg" />
       <h3 className="projectTitle">{item.title}</h3>
       <div className="projectDesc">{item.description}</div>
